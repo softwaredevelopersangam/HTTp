4888bffa85cb0956c09482ab775f166e826b9d11
softwaredevelopersangam <102554952+softwaredevelopersangam@users.noreply.github.com>
2022-09-24 21:08:20 +0530

    first commit

--- http.js
+++ http.js
@@ -1,8 +1,11 @@
 let cl = console.log;
 const postInfo = document.getElementById('postInfo');
-
+const postFormInfo = document.getElementById('postFormInfo');
+const title = document.getElementById('title');
+const info = document.getElementById('info');
 let baseUrl = 'https://jsonplaceholder.typicode.com/posts';
 
+let postArray = [];
 
 const genericMakeNetworkApiCall = (mName, url, body)=>{
     return new Promise((resolve, reject) =>{
@@ -12,8 +15,8 @@ const genericMakeNetworkApiCall = (mName, url, body)=>{
             if(xhr.status === 200 || xhr.status === 201){
                 resolve(xhr.response)
                 // console.log(xhr.response);
-                let data = JSON.parse(xhr.response);
-                templating(data);
+                // let data = JSON.parse(xhr.response);
+                // templating(data);
             }else{
                 reject('something went wrong');
             }
@@ -22,7 +25,11 @@ const genericMakeNetworkApiCall = (mName, url, body)=>{
     });
 };
 genericMakeNetworkApiCall('GET', baseUrl)
-    .then(res => cl(res))
+    .then(res => {
+        postArray = (JSON.parse(res));
+        templating(postArray);
+        localStorage.setItem('stdArray', JSON.stringify(postArray));
+    })
     .catch(cl)
 
 const templating = (arr)=>{
@@ -39,3 +46,22 @@ const templating = (arr)=>{
     });
     postInfo.innerHTML = result;
 };
+
+const onSubmitHandler = (e) =>{
+    e.preventDefault();
+    let obj ={
+        title : title.value,
+        body : info.value,
+        userId : Math.floor(Math.random()*10) + 1,
+    };
+    genericMakeNetworkApiCall('POST', baseUrl, JSON.stringify(obj))
+        .then(res => {
+            obj.id = JSON.parse(res).id;
+            postArray.push(obj);
+            templating(postArray);
+        })
+        .catch(cl)
+    e.target.reset();
+}
+
+postFormInfo.addEventListener('submit', onSubmitHandler);
